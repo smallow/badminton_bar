edfc88d6589bcd7f9cf672289c072140211e452b
wanghuidong <wanghuidong@topnet.net.cn>
2017-08-20 23:12:12 +0800

    init project

---
+++ src/main/java/com/smallow/BadmintonBarApplication.java
@@ -0,0 +1,12 @@
+package com.smallow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonBarApplication.class, args);
+	}
+}
