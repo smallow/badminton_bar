471d0e686a378e21de8e81ae540605866abd90a8
wanghuidong <wanghuidong@topnet.net.cn>
2017-09-08 11:54:04 +0800

    整合mybatis完成2---test类提交

--- src/test/java/com/smallow/service/impl/GroupServiceImplTest.java
+++ src/test/java/com/smallow/service/impl/GroupServiceImplTest.java
@@ -10,9 +10,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wanghuidong on 2017/8/31.
@@ -32,7 +36,7 @@ public class GroupServiceImplTest {
     public void save() throws Exception {
         Group badmintonGroup=new Group();
         //badmintonGroup.setGroupId(KeyUtil.genUniqueKey());
-        badmintonGroup.setGroupName("郑州羽飞扬俱乐部");
+        badmintonGroup.setGroupName("羽你同行");
         badmintonGroup.setGroupIcon("http://img.mp.itc.cn/upload/20160909/475a465d06d3476e8adfcae3e6c89062_th.jpg");
         //badmintonGroup.setGroupIcon("http://avatar.csdn.net/A/9/2/1_xiakepan.jpg");
         badmintonGroup.setGroupManagerIdNumber("41130219840716051X");
@@ -41,19 +45,25 @@ public class GroupServiceImplTest {
         badmintonGroup.setGroupCheck(BadmintonGroupCheckEnum.NEED.getCode());
         badmintonGroup.setGroupStatus(GroupStatusEnum.APPLY.getCode());
         badmintonGroup.setOpenid("oDUen0mqihpEtwribIeb_Muq5yzw");
-        badmintonGroup.setGroupMemo("临时接管俱乐部222");
-        int i=badmintonGroupService.save(badmintonGroup);
-        Assert.assertNotEquals(0,i);
+        badmintonGroup.setGroupMemo("天天找你");
+        badmintonGroupService.save(badmintonGroup);
+
     }
 
 
     @Test
     public void findList() throws Exception {
-        Group group=new Group();
-        group.setGroupManagerPhone("13603456869");
-        //group.setGroupName("郑州飞羽");
-        List<Group> list=badmintonGroupService.findList(group);
-        System.out.println(list);
+        PageRequest pageRequest=new PageRequest(0,2);
+        Map<String,Object> map=new HashMap<>();
+        map.put("groupManagerPhone","13603456869");
+        Page<Group> page=badmintonGroupService.findList(map,pageRequest);
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getTotalPages());
+        System.out.println(page.hasPrevious());
+        System.out.println(page.hasNext());
+        System.out.println(page.isFirst());
+        System.out.println(page.isLast());
+
     }
 
 }
